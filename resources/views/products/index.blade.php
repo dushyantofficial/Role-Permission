e9e0f43818ac4db3ee5186e5932083514988627c
dushyant <dushyantchhatraliya@gmail.com>
2023-11-01 15:02:20 +0530

    Responsive mobile design

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -126,8 +126,8 @@
         ">
                                                                 All fields are mandatory(*)
                                                             </div>
-                                                            <button type="button" class="btn-close"
-                                                                    data-bs-dismiss="modal" aria-label="Close">X
+                                                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                                                <span aria-hidden="true">&times;</span>
                                                             </button>
                                                         </div>
                                                         <div class="modal-body">
@@ -213,7 +213,8 @@
         ">
                                 All fields are mandatory(*)
                             </div>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">X
+                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div class="modal-body">
